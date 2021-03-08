17f2c3f84229eeb8e5d69ca700e35cab61dacfe2
Felix Chon <seoungyu@ualberta.ca>
2021-03-08 21:17:40 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: "UA-191537647-1",
       },
     },
     `gatsby-plugin-sitemap`,
